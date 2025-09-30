b952737433b367ff969ac32a901505d436576eec
Arabella Green <arabella.green@solirius.com>
2025-09-30 16:34:11 +0100

    Landing Page Tests

---
+++ tests/content/irregularHoursPage_content.ts
@@ -0,0 +1,6 @@
+const irregularHoursPage_content = {
+    pageTitle: "Calculate holiday entitlement",
+    // Continue here!
+} as const;
+
+export default irregularHoursPage_content
--- tests/test.spec.ts
+++ tests/test.spec.ts
@@ -5,6 +5,9 @@ test(`Page object model happy path for second test`, async ({ page }): Promise<v
     const landingPage: LandingPage = new LandingPage();
     await landingPage.checkPageLoads(page);
     await landingPage.continueOn(page);
+    // await irregularHoursPage.checkPageLoads(page);
+    // await irregularHoursPage.continueOn(page);
+    
     // Continue here!
 });
 // #Hello
