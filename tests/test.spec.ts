7e90a47540a7be321bb475db8b9eeb882078edbb
Arabella Green <arabella.green@solirius.com>
2025-10-01 11:27:09 +0100

    When does the leave year start?

--- tests/test.spec.ts
+++ tests/test.spec.ts
@@ -1,6 +1,7 @@
 import {test} from "@playwright/test";
 import LandingPage from "./pages/landingPage";
 import IrregularHoursPage from "./pages/irregularHoursPage";
+import LeaveYearPage from "./pages/leaveYearPage";
 
 test(`Page object model happy path for second test`, async ({ page }): Promise<void> => {
     const landingPage: LandingPage = new LandingPage();
@@ -8,6 +9,9 @@ test(`Page object model happy path for second test`, async ({ page }): Promise<v
     await landingPage.continueOn(page);
     const irregularHoursPage: IrregularHoursPage = new IrregularHoursPage();
     await irregularHoursPage.checkIrregularHoursPageLoads(page);
+    await irregularHoursPage.continueOn(page);
+    const leaveYearPage: LeaveYearPage = new LeaveYearPage();
+    await leaveYearPage.checkLeaveYearPageLoads(page);
     // await irregularHoursPage.continueOn(page);
     
     // Continue here!
